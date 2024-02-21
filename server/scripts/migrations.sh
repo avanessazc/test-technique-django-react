c60a8c3d314393909f980b85eaa5973e7f953aa6
ayzapata <ayzapata@student.42.fr>
2024-02-21 19:55:51 +0100

    Server: modified migrations.sh

--- server/scripts/migrations.sh
+++ server/scripts/migrations.sh
@@ -1,4 +1,1 @@
-python3 manage.py makemigrations
-python3 manage.py migrate
-python3 manage.py loaddata ./data/records.json
-python3 manage.py runserver 0.0.0.0:8000
+python3 manage.py makemigrations && python3 manage.py migrate && python3 manage.py loaddata ./data/records.json && python3 manage.py runserver 0.0.0.0:8000
